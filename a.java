174f2ed0fad126edf7dd1acc834e0b919b765a30
angadsarky <35285754+angadsarky@users.noreply.github.com>
2018-07-18 04:36:21 -0700

    Add files via upload

---
+++ a.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class a {
+
+	public static void main(String[] args) {
+		System.out.println("enter rows");
+		Scanner scan=new Scanner(System.in);
+		int rows;
+		rows=scan.nextInt();
+		double i,j,k,l;
+		for(i=0;i<=rows/2;i++)
+		{ for(j=rows+2;j>i;j--)
+			System.out.print(" ");
+		  for(k=1;k<=i;k++)
+			System.out.print("* ");
+		 System.out.println();
+		}
+		for(i=(rows/2)+1;i<=rows;i++)
+		{ l=i+1;
+
+			for(j=rows+1;j>i;j=j-1)
+			System.out.print(" ");
+		  for(k=1;k<=l;k=k+1)
+			{System.out.print("* ");
+                        }
+		 System.out.println();
+		l++;
+		}
+	}
+
+}
